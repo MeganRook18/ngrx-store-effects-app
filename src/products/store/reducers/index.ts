779d83df7cfc6a97a27fbc1464c655015bac16f1
MeganRook18 <megan-rook@malone-digital.com>
2020-06-11 15:53:24 +0100

    router state composition

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,11 +1,5 @@
-import {
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-} from "@ngrx/store";
-
+import { ActionReducerMap, createFeatureSelector } from "@ngrx/store";
 import * as fromPizzas from "./pizzas.reducer";
-import { parse } from "querystring";
 
 export interface ProductsState {
   pizzas: fromPizzas.PizzaState;
@@ -21,42 +15,3 @@ export const getProductsState = createFeatureSelector<ProductsState>(
   "products"
 );
 
-// pizzas state
-export const getPizzaState = createSelector(
-  getProductsState, // 1st arg is the high level of the state tree
-  (state: ProductsState) => state.pizzas // 2nd level of the state tree and return that product
-);
-
-export const getPizzasEntities = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasEntities // returns objects not array value
-);
-
-// return an array so component can loop over
-export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
-  return Object.keys(entities).map((id) => entities[parseInt(id, 10)]);
-});
-
-export const getPizzasLoaded = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoaded
-);
-
-export const getPizzasLoading = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoading
-);
-
-/* Application's State Tree
-
-const state = {
-    products: {
-        pizzas: { <--  the pizza reducer handdles this part of the tree
-            data: [],
-            loaded: false,
-            loading: true
-        }
-    }
-}
-
-*/
---
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -0,0 +1,54 @@
+import { createSelector } from "@ngrx/store";
+
+import * as fromRoot from "../../../app/store";
+import * as fromFeature from "../reducers";
+import * as fromPizzas from "../reducers/pizzas.reducer";
+
+import { Pizza } from "src/products/models/pizza.model";
+
+export const getPizzaState = createSelector(
+  fromFeature.getProductsState, // 1st arg is the high level of the state tree
+  (state: fromFeature.ProductsState) => state.pizzas // 2nd level of the state tree and return that product
+);
+
+export const getPizzasEntities = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasEntities // returns objects not array value
+);
+
+export const getSelectedPizza = createSelector(
+  getPizzasEntities, // feature state
+  fromRoot.getRouterState,
+  (entities, router): Pizza => {
+    return router.state && entities[router.state.params.pizzaId];
+  }
+);
+
+// return an array so component can loop over
+export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
+  return Object.keys(entities).map((id) => entities[parseInt(id, 10)]);
+});
+
+export const getPizzasLoaded = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoaded
+);
+
+export const getPizzasLoading = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoading
+);
+
+/* Application's State Tree
+  
+  const state = {
+      products: {
+          pizzas: { <--  the pizza reducer handdles this part of the tree
+              data: [],
+              loaded: false,
+              loading: true
+          }
+      }
+  }
+  
+  */
